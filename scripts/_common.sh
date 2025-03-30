3aa4f99eb95f8ed3684f15ed45259b4b7a5d6a66
oleole39 <59071673+oleole39@users.noreply.github.com>
2025-03-30 18:52:08 +0200

    fix go version var

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,4 +4,4 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-GO_VERSION="1.23.3" # match version defined in https://git.1e99.eu/1e99/passed/src/branch/main/go.mod
+go_version="1.23.3" # match version defined in https://git.1e99.eu/1e99/passed/src/branch/main/go.mod
