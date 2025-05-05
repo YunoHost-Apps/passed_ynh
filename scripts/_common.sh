2bafd3a502cfd3c084d2ff2c8f57ee4dadf274ef
oleole39 <59071673+oleole39@users.noreply.github.com>
2025-05-05 17:27:09 +0200

    upgrade go version

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,4 +4,4 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-go_version="1.23.3" # match version defined in https://git.1e99.eu/1e99/passed/src/branch/main/go.mod
+go_version="1.24.2" # match version defined in https://git.1e99.eu/1e99/passed/src/branch/main/go.mod
